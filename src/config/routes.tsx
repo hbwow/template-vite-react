233fd6e5f530d1b984b5fbb994ab600c228f5f4c
hbwow <lllengkaixin@gmail.com>
2024-01-04 17:41:47 +0800

    refactor: 💡 整理routes

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -2,28 +2,34 @@ import { createBrowserRouter } from 'react-router-dom';
 import loadable from '@loadable/component';
 
 import Layout from '@/layout';
-const Home = loadable(() => import('@/pages/home'));
-const Docs = loadable(() => import('@/pages/docs'));
 
-const routes = createBrowserRouter([
-  {
-    path: '/login',
-    element: <div>login</div>,
-  },
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/docs',
-        element: <Docs />,
-      },
-    ],
-  },
-]);
+import { Skeleton } from 'antd';
+
+const wrapLoadable = (dynamic: any) => {
+  return loadable(dynamic, {
+    fallback: <Skeleton active />,
+  });
+};
+
+const Home = wrapLoadable(() => import('@/pages/home'));
+const Docs = wrapLoadable(() => import('@/pages/docs'));
+
+// layout需要的数据结构
+export const layoutRoutes = {
+  path: '/',
+  element: <Layout />,
+  children: [
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/docs',
+      element: <Docs />,
+    },
+  ],
+};
+
+const routes = createBrowserRouter([layoutRoutes], { basename: import.meta.env.BASE_URL });
 
 export default routes;
