25559466bf5705c763f30627c5033ad4321f0d2e
hbwow <lllengkaixin@gmail.com>
2024-01-04 17:32:46 +0800

    refactor: 💡 环境变量 base

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
-import { defineConfig } from 'vite';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
@@ -7,30 +9,37 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 const BASE_URL = 'https://api.github.com'; // dev
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [{ src: 'node_modules/@hbwow/components/lib/pwsHtml/**/*', dest: 'pwsHtml' }],
-    }),
-    visualizer(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  server: {
-    port: 9090,
-    proxy: {
-      '/githubApi': {
-        target: BASE_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/githubApi/, ''),
-        // configure: (proxy, options) => {
-        // proxy will be an instance of 'http-proxy'
-        // },
+export default defineConfig(({ command, mode }) => {
+  // const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.VITE_BASENAME,
+
+    plugins: [
+      react(),
+      viteStaticCopy({
+        targets: [{ src: 'node_modules/@hbwow/components/lib/pwsHtml/**/*', dest: 'pwsHtml' }],
+      }),
+      visualizer(),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
       },
     },
-  },
+    server: {
+      port: 9090,
+      proxy: {
+        '/githubApi': {
+          target: BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/githubApi/, ''),
+          // configure: (proxy, options) => {
+          // proxy will be an instance of 'http-proxy'
+          // },
+        },
+      },
+    },
+  };
 });
