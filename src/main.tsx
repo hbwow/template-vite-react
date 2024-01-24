c02f38aff7f2fa06ad35340f78860e08b7e79d41
hbwow <lllengkaixin@gmail.com>
2024-01-24 17:07:38 +0800

    feat: 🎸 一些优化

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
 import { StyleProvider } from '@ant-design/cssinjs';
-import { Button } from 'antd';
+import { HandleInterceptorCodeCom } from '@hbwow/utils';
+import { ConfigProvider } from 'antd';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <StyleProvider hashPriority='high'>
-      {/* 👇 解决一些莫名其妙的antd样式问题，目前排查到的是HandleInterceptorCode里面用了antd model */}
-      <Button type='primary' className='absolute h-0 p-0 w-0 border-0' />
-      <App />
+      <ConfigProvider
+        theme={{
+          token: {
+            // colorPrimary: '#00b96b',
+            // colorInfo: '#00b96b',
+          },
+        }}
+      >
+        <App />
+        <HandleInterceptorCodeCom />
+      </ConfigProvider>
     </StyleProvider>
   </React.StrictMode>,
 );
